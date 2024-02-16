f15fec7a461f1d94693be5ee323b21490556b3e3
luiscaro11 <luiscarod86@gmail.com>
2024-02-16 08:40:35 -0400

    initial commit

---
+++ client/src/components/VehicleCards/VehicleAuctionCard/AuctionCountdown.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Countdown from "react-countdown";
+import PropTypes from "prop-types";
+import MKBadge from "components/MKBadge";
+import WatchLaterIcon from '@mui/icons-material/WatchLater';
+
+// Random component
+const Completionist = () =>
+  <MKBadge badgeContent={<>
+    <WatchLaterIcon />&nbsp;Closed
+  </>} color="success" container />;
+
+// Renderer callback with condition
+const renderer = ({ hours, minutes, seconds, completed }) => {
+  if (completed) {
+    // Render a complete state
+    return <Completionist />;
+  }
+  const time = <><WatchLaterIcon />&nbsp;{String(hours).padStart(2, "0")}:{String(minutes).padStart(2, "0")}:{String(seconds).padStart(2, "0")}</>
+  let color;
+  if(hours * 1 === 0 && minutes * 1 === 0)
+    color = 'error';
+  else color = 'warning';
+
+    // Render a countdown
+    return (
+      <MKBadge badgeContent={time} color={color} container />
+    );
+
+};
+
+function AuctionCountdown({timeEnd}){
+  return <Countdown date={timeEnd} renderer={renderer} />;
+}
+
+AuctionCountdown.propTypes = {
+  timeEnd: PropTypes.instanceOf(Date).isRequired
+};
+
+export default AuctionCountdown;
